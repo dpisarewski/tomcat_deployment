d4f43d08d305312b9a1ce118bfdb2b5c7b5d685d
Dieter Pisarewski <dieter.pisarewski@arvatosystems.com>
2012-05-15 15:35:36 -0400

    Ignore error if is not able to remove old files

--- lib/tomcat_deployment/recipe.rb
+++ lib/tomcat_deployment/recipe.rb
@@ -207,7 +207,7 @@ Capistrano::Configuration.instance.load do
     end
 
     task :remove_old_application_directory do
-      sudo "sh -c 'rm -rf #{webapps_directory}/#{application}'", :as => tomcat_user
+      sudo "sh -c 'rm -rf #{webapps_directory}/#{application}'; true", :as => tomcat_user
     end
 
     desc "tail :tomcat_home/logs/*.log and logs/catalina.out"
@@ -329,7 +329,7 @@ Capistrano::Configuration.instance.load do
 
   namespace :deploy_user do
     task :configure do |t|
-      run "[ $SHELL != '/bin/bash' ] && chsh -s /bin/bash || true" do |channel, stream, data|
+      run "[ $SHELL != '/bin/bash' ] && chsh -s /bin/bash; true" do |channel, stream, data|
         if data =~ /^Password:/
           logger.info "#{channel[:host]} asked for password"
           channel.send_data password
